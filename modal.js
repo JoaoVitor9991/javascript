4c673e28fdbb6013326bbd2ff3f1fd7a8c88cffb
joão Vitor <joaovitormachadodev99@gmail.com>
2025-06-24 17:38:18 -0300

    tt

--- modal.js
+++ modal.js
@@ -5,6 +5,11 @@ let BtnCloseModal = document.getElementById('BtnCloseModal')
 
 let Modal = document.getElementById('Modal')
 
+
+
+
+
+
 BtnOpenModal.addEventListener('click', () =>{
     Modal.classList.add('ModalOpen')
 })
