018ac7402c01b08552c8d0876a9a14e48ce0ecd0
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-01 16:02:33 -0300

    att

--- aula_thiago/js/script.js
+++ aula_thiago/js/script.js
@@ -8,11 +8,26 @@ var botao_mais = document.getElementById("btn_plus");
 var botao_menos = document.getElementById("btn_less");
 var botao_click = document.getElementById("btn_click");
 
+var header = document.querySelector(".cabecalho");
+
+header.addEventListener('mouseover',  function(){
+    header.style.backgroundColor = 'black';
+    header.style.color = '#3519e6'
+})
+
+
+
+
 
 function mudacor(){
     //alert("Muda cor");
-    var div = document.querySelector("div_texto");
+    var div = document.querySelector(".div_texto");
     //var div2 = document.getElementsByClassName("div_texto");
+    var container = document.getElementById("container_btns");
+
+
+    div.style.backgroundColor = "yellow";
+    container.style.backgroundColor = "blue";
 }
 
 function postar(){
