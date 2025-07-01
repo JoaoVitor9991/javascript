30a49390079f4f65e1379fa46f784e448ef71470
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-01 16:24:57 -0300

    att

--- aula_thiago/js/script.js
+++ aula_thiago/js/script.js
@@ -8,6 +8,10 @@ var botao_mais = document.getElementById("btn_plus");
 var botao_menos = document.getElementById("btn_less");
 var botao_click = document.getElementById("btn_click");
 
+var contador = document.querySelector("#texto").textContent;
+
+console.log(contador);
+
 var header = document.querySelector(".cabecalho");
 
 header.addEventListener('mouseover',  function(){
@@ -15,6 +19,10 @@ header.addEventListener('mouseover',  function(){
     header.style.color = '#3519e6'
 })
 
+header.addEventListener('mouseout',  function(){
+    header.style.backgroundColor = '#3519e6';
+    header.style.color = ' black'
+})
 
 
 
