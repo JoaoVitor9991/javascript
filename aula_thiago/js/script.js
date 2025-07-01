65c8c5c7f04d4c1ac4307d6442715473737b6289
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-01 15:27:45 -0300

    att

--- aula_thiago/js/script.js
+++ aula_thiago/js/script.js
@@ -3,6 +3,12 @@
 console.log("Mensagem no console");
 //essa função é similar ao print no python
 
-var btn_color = document.getElementById("btn_color");
+var btn_cor = document.getElementById("btn_color");
+var btn_mais = document.getElementById("btn_plus");
+var btn_menos = document.getElementById("btn_less");
+var btn_click = document.getElementById("btn_click");
 
-console.log(btn_color);
+console.log(btn_cor);
+console.log(btn_menos);
+console.log(btn_mais);
+console.log(btn_click);
