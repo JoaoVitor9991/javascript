286ba0b10364fcb33d73f994dc6a17b7d13835ed
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-01 18:14:59 -0300

    att

---
+++ form/js/script.js
@@ -0,0 +1,13 @@
+//let input_nome = document.getElementById("nome");
+//let span = document.getElementById("msg");
+
+//input_nome.addEventListener('keyup',function(event){
+  //  console.log(event.target.value)
+//})
+
+let formulario = document.getElementById("formulario");
+
+formulario.equal.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('clicou')
+})
