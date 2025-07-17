7ef646e72b9bfae3690c8232624fe6f26e2b6f19
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-17 17:29:57 -0300

    att

--- js_validacao/script.js
+++ js_validacao/script.js
@@ -1,10 +1,25 @@
 const inputs = document.querySelectorAll('.required');
 
+
+const spans = document.querySelectorAll('.span-required');
+
+
+
 function nameValidate(){
     if (inputs[0].value.length<3){
         console.log('Nome deve ter 3 caracteres');
+            SetError(0);
     }
     else{
-        console.log('Validado com sucesso!');
+        removeError(0);
     }
+
+function SetError(index){
+    spans[index].style.display = 'block';
+} 
+function removeError(index){
+    spans[index].style.display = 'none'
 }
+
+}
+
