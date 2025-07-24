1c1f3840472d36a1e584e3a94a25c2ccd22d5de0
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-24 15:54:40 -0300

    att

--- expressao/script.js
+++ expressao/script.js
@@ -2,4 +2,15 @@
 
 //document.write(/^\d{2}\/\d{2}\/\d{4}$/.test('33/33/3333'))
 
-let dia = /^(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
+//let dia = /^(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
+
+//document.write(dia.test("32"))
+
+//document.write(dia.test("31"))
+
+
+let ano = /^\d{4}$/;
+
+document.write(ano.test("24"))
+
+document.write(ano.test("2024"))
