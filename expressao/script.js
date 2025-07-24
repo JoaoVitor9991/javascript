82619a1d84a8a90ce5f72154f02ab7e26d974099
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-24 15:44:44 -0300

    att

--- expressao/script.js
+++ expressao/script.js
@@ -1,3 +1,5 @@
 // document.write(/[abc]/.test('abc'))
 
-document.write(/^\d{2}\/\d{2}\/\d{4}$/.test('33/33/3333'))
+//document.write(/^\d{2}\/\d{2}\/\d{4}$/.test('33/33/3333'))
+
+let dia = /^(0[1-9]|1[0-9]|2[0-9]|3[0-1])$/;
