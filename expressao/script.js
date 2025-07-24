ab01eaf0589867ee6390637798606c0ccac0daa1
joão Vitor <joaovitormachadodev99@gmail.com>
2025-07-24 16:07:40 -0300

    att

--- expressao/script.js
+++ expressao/script.js
@@ -9,8 +9,17 @@
 //document.write(dia.test("31"))
 
 
-let ano = /^\d{4}$/;
+//let ano = /^\d{4}$/;
 
-document.write(ano.test("24"))
+//document.write(ano.test("24"))
 
-document.write(ano.test("2024"))
+//document.write(ano.test("2024"))
+
+
+//let data = /^(0[1-9]|1[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/d{4}$/;
+
+//document.write(data.test("22/04/2120"))
+
+//let cpf = /^[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2})$/;
+
+//document.write(cpf.test("957;488.555-52"))//
